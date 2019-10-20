1e77ccf894931a104c4b93e3dceb05fd9fde045f
wanghongen <wanghongen@e-sscard.com>
2019-10-20 14:52:24 +0800

    add .

--- ace-common/src/main/java/com/github/wxiaoqi/security/common/rest/BaseController.java
+++ ace-common/src/main/java/com/github/wxiaoqi/security/common/rest/BaseController.java
@@ -20,54 +20,57 @@ import java.util.Map;
  * @create 2017-06-15 8:48
  */
 @Slf4j
-public class BaseController<Biz extends BaseBiz,Entity> {
+public class BaseController<Biz extends BaseBiz, Entity> {
     @Autowired
     protected HttpServletRequest request;
     @Autowired
     protected Biz baseBiz;
 
-    @RequestMapping(value = "",method = RequestMethod.POST)
+    @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
-    public ObjectRestResponse<Entity> add(@RequestBody Entity entity){
+    public ObjectRestResponse<Entity> add(@RequestBody Entity entity) {
         baseBiz.insertSelective(entity);
         return new ObjectRestResponse<Entity>();
     }
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public ObjectRestResponse<Entity> get(@PathVariable int id){
+    public ObjectRestResponse<Entity> get(@PathVariable int id) {
         ObjectRestResponse<Entity> entityObjectRestResponse = new ObjectRestResponse<>();
         Object o = baseBiz.selectById(id);
-        entityObjectRestResponse.data((Entity)o);
+        entityObjectRestResponse.data((Entity) o);
         return entityObjectRestResponse;
     }
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @ResponseBody
-    public ObjectRestResponse<Entity> update(@RequestBody Entity entity){
+    public ObjectRestResponse<Entity> update(@RequestBody Entity entity) {
         baseBiz.updateSelectiveById(entity);
         return new ObjectRestResponse<Entity>();
     }
-    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    public ObjectRestResponse<Entity> remove(@PathVariable int id){
+    public ObjectRestResponse<Entity> remove(@PathVariable int id) {
         baseBiz.deleteById(id);
         return new ObjectRestResponse<Entity>();
     }
 
-    @RequestMapping(value = "/all",method = RequestMethod.GET)
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     @ResponseBody
-    public List<Entity> all(){
+    public List<Entity> all() {
         return baseBiz.selectListAll();
     }
-    @RequestMapping(value = "/page",method = RequestMethod.GET)
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
     @ResponseBody
-    public TableResultResponse<Entity> list(@RequestParam Map<String, Object> params){
+    public TableResultResponse<Entity> list(@RequestParam Map<String, Object> params) {
         //查询列表数据
         Query query = new Query(params);
         return baseBiz.selectByQuery(query);
     }
-    public String getCurrentUserName(){
+
+    public String getCurrentUserName() {
         return BaseContextHandler.getUsername();
     }
 }
